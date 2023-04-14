82c2c4e9d1e756cab056aa655cfec2966a535d5f
silviabenfenati <silviamaria.benfenati@gmail.com>
2023-04-14 17:08:11 +0200

    requirements.txtx

--- fiori/app1.py
+++ fiori/app1.py
@@ -7,6 +7,7 @@ import numpy as np
 st.title('**Iris database analysis**')
 
 df_fiori = pd.read_csv("iris.csv")
+
 st.dataframe(df_fiori)
 
 #background
@@ -21,5 +22,11 @@ def add_bg_from_url():
          }}
          </style>
          """,
+         
          unsafe_allow_html=True
      )
+add_bg_from_url() 
+
+audio_file = open("rick.mp3", "rb")
+audio_bytes = audio_file.read()
+st.audio(audio_bytes, format="audio/mp3")
